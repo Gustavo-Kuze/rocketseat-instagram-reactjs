0d4a877f47631f95101709f0b3a920d88aa8e46e
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-06-12 13:43:37 -0300

    Iniciada a criação do componente Feed 27:04

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,12 +1,27 @@
+import './Feed.css'
 import React, { Component } from 'react'
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
 
+class Feed extends Component {
 
-class Feed extends Component{
-    
-    render(){
-        return <h1>
-            Feed
-        </h1>
+    render() {
+        return (
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Apenas um Teste</span>
+                            <span className="place">Portão, RS</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+                    <img src="" alt="Postagem"/>
+                </article>
+            </section>
+        );
     }
 }
 
