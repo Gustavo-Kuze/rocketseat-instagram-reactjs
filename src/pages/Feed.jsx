5187b49b5ad54e5ad09f54c47d31f08b3a2bc198
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-06-13 09:11:41 -0300

    Listagem de posts vindo do back; Estilização página New

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,37 +1,49 @@
-import './Feed.css'
-import React, { Component } from 'react'
-import more from '../assets/more.svg'
-import like from '../assets/like.svg'
-import comment from '../assets/comment.svg'
-import send from '../assets/send.svg'
+import './Feed.css';
+import React, { Component } from 'react';
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+import api from '../services/api';
 
 class Feed extends Component {
+
+    state = {
+        feed: []
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({ feed: response.data })
+    }
 
     render() {
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Apenas um Teste</span>
-                            <span className="place">Portão, RS</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-                    <img src="https://image.freepik.com/fotos-gratis/3d-render-medico-imagem-macho-figura-cerebro-destacado_1048-5873.jpg" alt="Postagem" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="" />
-                            <img src={comment} alt="" />
-                            <img src={send} alt="" />
-                        </div>
-
-                        <strong>900 curtidas</strong>
-                        <p>Um post massa!
-                        <span>#teste #top</span>
-                        </p>
-                    </footer>
-                </article>
+                {
+                    this.state.feed.map((post) => (<article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+                            <img src={more} alt="Mais" />
+                        </header>
+                        <img src={`https://localhost:3333/files/${post.image}`} alt="Postagem" />
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+    
+                            <strong>{post.likes} curtidas</strong>
+                            <p>{post.description}
+                            <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>))
+                }
             </section>
         );
     }
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,12 +1,21 @@
+import './New.css'
 import React, { Component } from 'react'
 
+class New extends Component {
 
-class New extends Component{
-    
-    render(){
-        return <h1>
-            New
-        </h1>
+    render() {
+        return (
+            <form id="new-post">
+                <input type="file" />
+
+                <input type="text" name="author" placeholder="Autor do post" />
+                <input type="text" name="place" placeholder="Local do post" />
+                <input type="text" name="description" placeholder="Descrição do post" />
+                <input type="text" name="hashtags" placeholder="Hashtags do post" />
+
+                <button type="submit">Enviar</button>
+            </form>
+        )
     }
 }
 
